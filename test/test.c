4bcbf61db66c4e54d91a8df23ce57681307ae2de
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-23 03:54:48 +0530

    basic timeut test case added

--- test/test.c
+++ test/test.c
@@ -105,16 +105,16 @@ void* tb4_func(void* p)
 
 uint64_t millis_since_start()
 {
-    static struct timespec start = {0};
+	static struct timespec start = {0};
 
-    if (start.tv_sec == 0 && start.tv_nsec == 0)
-        timespec_get(&start, CLOCK_MONOTONIC);
+	if (start.tv_sec == 0 && start.tv_nsec == 0)
+		timespec_get(&start, CLOCK_MONOTONIC);
 
-    struct timespec now;
-    timespec_get(&now, CLOCK_MONOTONIC);
+	struct timespec now;
+	timespec_get(&now, CLOCK_MONOTONIC);
 
-    return (now.tv_sec - start.tv_sec) * 1000LL +
-           (now.tv_nsec - start.tv_nsec) / 1000000LL;
+	return (now.tv_sec - start.tv_sec) * 1000LL +
+			(now.tv_nsec - start.tv_nsec) / 1000000LL;
 }
 
 void* tb5_func(void* p)
@@ -141,12 +141,88 @@ void* tb6_func(void* p)
 	return NULL;
 }
 
+tiber_mutex lock1;
+tiber_cond wait1;
+
+void* tb7_func(void* p)
+{
+	tiber_mutex_lock(&lock1);
+	printf("Locked for task 7 @ %lu\n", millis_since_start());
+
+	tiber_msleep(3000);
+
+	tiber_mutex_unlock(&lock1);
+	printf("Unlocked for task 7 @ %lu\n", millis_since_start());
+
+	tiber_mutex_lock(&lock1);
+	printf("Locked for task 7 @ %lu\n", millis_since_start());
+
+	WAKEUP(&wait1);
+
+	tiber_mutex_unlock(&lock1);
+	printf("Unlocked for task 7 @ %lu\n", millis_since_start());
+
+	return NULL;
+}
+
+void* tb8_func(void* p)
+{
+	tiber_msleep(10);
+
+	{
+		struct timespec wait_until;
+		timespec_get(&wait_until, CLOCK_MONOTONIC);
+		wait_until = timespec_add(wait_until, timespec_from_milliseconds(2000));
+
+		int result = tiber_mutex_timedlock(&lock1, &wait_until);
+		if(result == ETIMEDOUT)
+			printf("Lock timedout for task 8 @ %lu\n", millis_since_start());
+		else
+		{
+			printf("ERROR, this must timeout for task 8 @ %lu\n", millis_since_start());
+			exit(-1);
+		}
+	}
+
+	{
+		struct timespec wait_until;
+		timespec_get(&wait_until, CLOCK_MONOTONIC);
+		wait_until = timespec_add(wait_until, timespec_from_milliseconds(2000));
+
+		int result = tiber_mutex_timedlock(&lock1, &wait_until);
+		if(result == ETIMEDOUT)
+			printf("ERROR, this must not timeout for task 8 @ %lu\n", millis_since_start());
+		else
+		{
+			printf("Locked for task 8 @ %lu\n", millis_since_start());
+			exit(-1);
+		}
+	}
+
+	{
+		struct timespec wait_until;
+		timespec_get(&wait_until, CLOCK_MONOTONIC);
+		wait_until = timespec_add(wait_until, timespec_from_milliseconds(2000));
+
+		int result = tiber_cond_timedwait(&wait1, &lock1, &wait_until);
+		printf("Woken up with ((result == ETIMEDOUT) -> %d, %d) for task 8 @ %lu\n", (result == ETIMEDOUT), result, millis_since_start());
+	}
+
+	tiber_mutex_unlock(&lock1);
+	printf("Unlocked for task 8 @ %lu\n", millis_since_start());
+
+	return NULL;
+}
+
 int main()
 {
 	millis_since_start();
 
 	tiber_mutex_init(&lock);
 	tiber_cond_init(&wait);
+
+	tiber_mutex_init(&lock1);
+	tiber_cond_init(&wait1);
 
 	tiber_runtime* tr = new_tiber_runtime(RUNTIME_THREADS_COUNT, STACK_SIZE);
 
@@ -156,12 +232,17 @@ int main()
 	tiber* tb4 = new_tiber(tr, tb4_func, NULL, 4096);
 	tiber* tb5 = new_tiber(tr, tb5_func, NULL, 4096);
 	tiber* tb6 = new_tiber(tr, tb6_func, NULL, 4096);
+	tiber* tb7 = new_tiber(tr, tb7_func, NULL, 4096);
+	tiber* tb8 = new_tiber(tr, tb8_func, NULL, 4096);
 
 	// wait for 5 seconds
 	sleep(10);
 
 	tiber_mutex_destroy(&lock);
 	tiber_cond_destroy(&wait);
+
+	tiber_mutex_destroy(&lock1);
+	tiber_cond_destroy(&wait1);
 
 	return 0;
 }
