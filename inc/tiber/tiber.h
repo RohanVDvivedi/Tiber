c0e0e437848a0259ad4fa0b3a6bf02b192cb1c50
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-22 18:29:26 +0530

    basic structural layout for the function completed

--- inc/tiber/tiber.h
+++ inc/tiber/tiber.h
@@ -1,20 +1,46 @@
 #ifndef TIBER_H
 #define TIBER_H
 
-#include<stdint.h>
+#include<cutlery/pheap.h>
+#include<cutlery/linkedlist.h>
+#include<boompar/executor.h>
+#include<boompar/alarm_job.h>
 
-#include<pthread.h>
+#include<posixutils/timespec_utils.h>
+
 #include<ucontext.h>
 
-#include<cutlery/linkedlist.h>
+#include<pthread.h>
 
-#include<boompar/executor.h>
+typedef struct tiber_runtime tiber_runtime;
+struct tiber_runtime
+{
+	executor* tiber_executor;
 
-/*
-	This is simple coroutine/fiber library built using the support of ucontext underneath
-	I would humbly request you to use a UNBOUNDED_SYNC_QUEUE / JOB_QUEUE_AS_LINKEDLIST with a FIXED_THREAD_COUNT_EXECUTOR (thread pool size equal to number of hardware threads and stack size of 24KB) with this runtime
-	you can use mutex from pthread, but substitute pthread_cond_t with tiber_cond for condition variables while using this runtime
-*/
+	alarm_job* tiber_timer_job;
+
+	pthread_spinlock_t tiber_timer_lock;
+
+	pheap tiber_timer_queue;
+};
+
+typedef struct tiber_mutex tiber_mutex;
+struct tiber_mutex
+{
+	pthread_spinlock_t lock;
+
+	linkedlist waiting_tibers;
+
+	int is_locked;
+};
+
+typedef struct tiber_cond tiber_cond;
+struct tiber_cond
+{
+	pthread_spinlock_t lock;
+
+	linkedlist waiting_tibers;
+};
 
 typedef enum tiber_state tiber_state;
 enum tiber_state
@@ -28,9 +54,6 @@ enum tiber_state
 typedef struct tiber tiber;
 struct tiber
 {
-	// this is the thread_pool it will run on
-	executor* thread_pool;
-
 	// free this stack memory on TIBER_KILLED state
 	void* stack;
 
@@ -38,50 +61,22 @@ struct tiber
 	pthread_spinlock_t tiber_context_lock;
 	ucontext_t tiber_context;
 
-	// ucontext of the thread that called this tiber
-	// swap back to this when not able to run
-	// this is the parent thread that called this tiber, so it is also protected by the tiber_context_lock
-	ucontext_t tiber_caller;
-
 	// tiber's state
 	pthread_spinlock_t tiber_state_lock;
 	tiber_state state;
 
-	// protected by the tiber_cond_lock
+	// for tiber_cond
+	tiber_cond* waiting_on_tiber_cond;
 	llnode embed_node_for_tiber_cond_waiters;
+
+	// for tiber_mutex
+	tiber_mutex* waiting_on_tiber_mutex;
+	llnode embed_node_for_tiber_mutex_waiters;
+
+	// this will be set of the 
+	struct timespec abstime_for_wakeup;
+	phpnode embed_node_for_tiber_timer_queue;
+	int is_timer_set;
 };
-
-
-// initializes tiber in queued state, and pushes it into the job queue for the thread_pool
-int initialize_and_run_tiber(tiber* tb, executor* thread_pool, void (*entry_func)(void* input_p), void* input_p, uint64_t stack_size);
-
-// must be called from within the tiber
-void yield_tiber();
-
-// must be called from within the tiber
-void kill_tiber();
-
-void deinitialize_tiber(tiber* tb);
-
-typedef struct tiber_cond tiber_cond;
-struct tiber_cond
-{
-	pthread_spinlock_t lock;
-
-	// all the tibers wait on this list
-	linkedlist tiber_cond_waiters;
-};
-
-// has api very similar to the pthread_cond_t
-
-void tiber_cond_init(tiber_cond* tc);
-
-void tiber_cond_wait(tiber_cond* tc, pthread_mutex_t* m);
-
-void tiber_cond_signal(tiber_cond* tc);
-
-void tiber_cond_broadcast(tiber_cond* tc);
-
-void tiber_cond_destroy(tiber_cond* tc);
 
 #endif
---
+++ src/tiber_runtime.c
@@ -0,0 +1,67 @@
+#include<tiber/tiber.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// runtime for this thread is stored here
+__thread tiber_runtime* thread_runtime = NULL;
+
+// the current tiber that this thread is executing get's stored here
+__thread tiber* curr_tiber = NULL;
+
+// the context that this tiber must return to is stored here for each of the threads, it is the context of the thread that this tiber must return to after execution
+__thread ucontext_t thread_context;
+
+/*
+	puts tiber as thread local variable of this thread
+	puts tiber in running state
+	takes it's context lock
+	swaps context running the tiber
+	releases it's context lock
+	returns
+
+	** only this function takes the context_lock of the tiber
+*/
+static void* tiber_execute_wrapper(void* tb_v)
+{
+	// set the thread local
+	curr_tiber = (tiber*)tb_v;
+
+	// change curr_tiber's state to running
+	pthread_spin_lock(&(curr_tiber->tiber_state_lock));
+	if(curr_tiber->state == TIBER_QUEUED)
+		curr_tiber->state = TIBER_RUNNING;
+	else
+	{
+		printf("TIBER BUG: tiber was queued to thread pool but is not in queued state\n");
+		exit(-1);
+	}
+	pthread_spin_unlock(&(curr_tiber->tiber_state_lock));
+
+	// run curr_tiber, we only use this lock here and nowhere else
+	pthread_spin_lock(&(curr_tiber->tiber_context_lock));
+	if(-1 == getcontext(&thread_context))
+	{
+		printf("TIBER BUG: thread_context could not be populated\n");
+		exit(-1);
+	}
+	curr_tiber->tiber_context.uc_link = &thread_context;
+	if(-1 == swapcontext(&thread_context, &(curr_tiber->tiber_context)))
+	{
+		printf("TIBER BUG: tiber could not context switch into itself\n");
+		exit(-1);
+	}
+	pthread_spin_unlock(&(curr_tiber->tiber_context_lock));
+
+	// change curr_tiber's state if running to killed
+	// because it returned from the entry function
+	pthread_spin_lock(&(curr_tiber->tiber_state_lock));
+	if(curr_tiber->state == TIBER_RUNNING)
+		curr_tiber->state = TIBER_KILLED;
+	pthread_spin_unlock(&(curr_tiber->tiber_state_lock));
+
+	// reset the thread local
+	curr_tiber = NULL;
+
+	return NULL;
+}
