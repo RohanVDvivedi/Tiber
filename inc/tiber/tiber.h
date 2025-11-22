f2cbb191ffde9f17dab5724a7809b4a04877c345
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-22 18:55:41 +0530

    tiber api finalized

--- inc/tiber/tiber.h
+++ inc/tiber/tiber.h
@@ -12,6 +12,8 @@
 
 #include<pthread.h>
 
+// tiber runtime
+
 typedef struct tiber_runtime tiber_runtime;
 struct tiber_runtime
 {
@@ -24,6 +26,12 @@ struct tiber_runtime
 	pheap tiber_timer_queue;
 };
 
+tiber_runtime* new_tiber_runtime(uint64_t thread_count, uint64_t stack_size);
+
+void delete_tiber_runtime(tiber_runtime* tr_p);
+
+// tiber mutex
+
 typedef struct tiber_mutex tiber_mutex;
 struct tiber_mutex
 {
@@ -34,6 +42,15 @@ struct tiber_mutex
 	int is_locked;
 };
 
+int tiber_mutex_init(tiber_mutex* tm);
+int tiber_mutex_destroy(tiber_mutex* tm);
+int tiber_mutex_lock(tiber_mutex* tm);
+int tiber_mutex_trylock(tiber_mutex* tm);
+int tiber_mutex_timedlock(tiber_mutex* tm, const struct timespec *abs_time);
+int tiber_mutex_unlock(tiber_mutex* tm);
+
+// tiber condtion variable
+
 typedef struct tiber_cond tiber_cond;
 struct tiber_cond
 {
@@ -41,6 +58,15 @@ struct tiber_cond
 
 	linkedlist waiting_tibers;
 };
+
+int tiber_cond_init(tiber_cond* tc);
+int tiber_cond_destroy(tiber_cond* tc);
+int tiber_cond_wait(tiber_cond* tc, tiber_mutex* tm);
+int tiber_cond_timedwait(tiber_cond* tc, tiber_mutex* tm, const struct timespec *abs_time);
+int tiber_cond_signal(tiber_cond* tc);
+int tiber_cond_broadcast(tiber_cond* tc);
+
+// tiber itself
 
 typedef enum tiber_state tiber_state;
 enum tiber_state
@@ -64,6 +90,7 @@ struct tiber
 	// tiber's state
 	pthread_spinlock_t tiber_state_lock;
 	tiber_state state;
+	void* return_value;
 
 	// for tiber_cond
 	tiber_cond* waiting_on_tiber_cond;
@@ -78,5 +105,20 @@ struct tiber
 	phpnode embed_node_for_tiber_timer_queue;
 	int is_timer_set;
 };
+
+// if tr_p is NULL, the tiber is created for the current runtime
+tiber* new_tiber(tiber_runtime* tr_p, void* (*entry_func)(void* input_p), void* input_p, uint64_t stack_size);
+
+// only the below 2 functions actually delete the tiber object
+int tiber_join(tiber* tb, void** return_value);
+int tiber_detach(tiber* tb);
+
+tiber* tiber_self(void);
+
+void tiber_exit(void);
+void tiber_yield(void);
+
+void tiber_sleep(const struct timespec *abs_time);
+
 
 #endif
