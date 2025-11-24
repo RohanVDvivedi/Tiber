de22f7eccca0f748f8a644335a763a0e40f360df
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-25 00:20:43 +0530

    compiling tests with -O3

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,2 +1,2 @@
-gcc ./test.c -o test.out -ltiber -lconnman -lboompar -lcutlery
-gcc ./test2.c -o test2.out -ltiber -lconnman -lboompar -lcutlery
+gcc -O3 ./test.c -o test.out -ltiber -lconnman -lboompar -lcutlery
+gcc -O3 ./test2.c -o test2.out -ltiber -lconnman -lboompar -lcutlery
