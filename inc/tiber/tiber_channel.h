f8c427757dd837acece0c12a9f1a9cc8136f2372
RohanVDvivedi <rohandvivedi@gmail.com>
2026-02-16 20:34:10 +0530

    tiber channel header added

---
+++ inc/tiber/tiber_channel.h
@@ -0,0 +1,45 @@
+#ifndef TIBER_CHANNEL_H
+#define TIBER_CHANNEL_H
+
+#include<cutlery/dpipe.h>
+
+#include<tiber/tiber.h>
+
+/*
+	tiber_channel is an abstraction of a concurrent byte buffer that can be used for communication agnostically,
+	without knowing whether the communicating entities are tibers or pthreads or both
+*/
+
+#define UNBOUNDED_TIBER_CHANNEL_CAPACITY CY_UINT_MAX
+
+typedef struct tiber_channel tiber_channel;
+struct tiber_channel
+{
+	dpipe channel;
+
+	// if equals UNBOUNDED_TIBER_CHANNEL_CAPACITY
+	// the it is unbounded
+	cy_uint max_capacity;
+
+	tiber_mutex lock;
+
+	tiber_cond full_wait;
+
+	tiber_cond empty_wait;
+};
+
+int initialize_tiber_channel(tiber_channel* tch, cy_uint max_capacity);
+
+cy_uint write_to_tiber_channel(tiber_channel* tch, const void* data, cy_uint data_size, dpipe_operation_type op_type, uint64_t timeout_in_microseconds);
+
+cy_uint read_from_tiber_channel(tiber_channel* tch, void* data, cy_uint data_size, dpipe_operation_type op_type, uint64_t timeout_in_microseconds);
+
+cy_uint get_bytes_tiber_channel(const tiber_channel* tch);
+
+void close_tiber_channel(tiber_channel* tch);
+
+int is_closed_tiber_channel(const tiber_channel* tch);
+
+void deinitialize_tiber_channel(tiber_channel* tch);
+
+#endif
